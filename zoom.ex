8ac90a9c9504ebd2fc2fea76f254e4d8e34effc7
Alex Beeken <aeb242@gmail.com>
2017-11-22 17:45:36 -0800

    new approach - building the new image one space at a time

--- zoom.ex
+++ zoom.ex
@@ -1,19 +1,52 @@
 defmodule Zoom do
-  def expand([ h | t ], reference) do
-    square =
-      case h do
-        "#" -> [["#", "#"],["#","#"]]
-        " " -> [[" ", " "],[" "," "]]
-      end
-    [ square | expand(t, reference) ]
+  @h "#"
+  @s " "
+
+  def get_parent(image, x, y) do
+    from_coords(image, div(x, 2), div(y, 2))
   end
 
-  def expand([], _reference) do
+  def expand_blank(image) do
+    List.duplicate(
+      List.duplicate(
+        @s,
+        width(image) * 2
+      ),
+      height(image) * 2
+    )
+  end
+
+  def from_coords(image, x, y) do
+    { :ok, row } = Enum.fetch(image, y)
+    { :ok, char } = Enum.fetch(row, x)
+    char
+  end
+
+  def next_x(original, x) do
+    rem((x + 1), (width(original) * 2))
+  end
+
+  def populate_blanks([ h | t ], original, x, y) do
+    [ populate_row(h, original, 0, y) | populate_blanks(t, original, 0, y + 1) ]
+  end
+
+  def populate_blanks([], original, x, y) do
     []
   end
 
-  def slice(array, height, width) do
-    Enum.chunk_every(array, width)
+  def populate_row([ h | t ], original, x, y) do
+    parent = get_parent(original, x, y)
+    new_char = if (parent == @h) do
+                 @h
+               else
+                 @s
+               end
+    x = next_x(original, x)
+    [ new_char | populate_row(t, original, x, y) ]
+  end
+
+  def populate_row([], original, x, y) do
+    []
   end
 
   def split([ h | t ]) do
@@ -26,18 +59,17 @@ defmodule Zoom do
 
   def width(image) do
     {:ok, row } = Enum.fetch(image, 0)
-    String.length(row)
+    length(row)
+  end
+
+  def height(image) do
+    length(image)
   end
 
   def zoom(image) do
-    flat =
-      image
-      |> split
-      |> List.flatten
-
-    flat
-    |> expand(flat)
-    |> slice(0, width(image))
+    image = split(image)
+    blanks = expand_blank(image)
+    populate_blanks(blanks, image, 0, 0)
   end
 end
 
