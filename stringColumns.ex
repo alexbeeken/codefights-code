232897e951570f9a36c6d3f5c21e41d04b30018f
Alex Beeken <aeb242@gmail.com>
2017-11-28 00:02:31 -0800

    new appraoch, last one was too confusing so I wiped the slate clean

--- stringColumns.ex
+++ stringColumns.ex
@@ -1,75 +1,62 @@
 require IEx
 
 defmodule StringColumns do
+  def num_rows(sorted) do
+    length(sorted)/3
+      |> Float.ceil
+      |> round
+  end
+
+  def num_columns(sorted) do
+    case length(sorted) do
+      1 -> 1
+      2 -> 2
+      x -> 3
+    end
+  end
+
+  def reconstruct([ h | t ]) do
+    if length(h) == 3 do
+      Enum.join(h, " ") <> "\n" <> reconstruct(t)
+    else
+      Enum.join(h, " ")
+    end
+  end
+
+  def reconstruct(_) do "" end
+
+  def rotate(columns, num_rows, current_row_idx, output) do
+    if current_row_idx < num_rows do
+      [ col1, col2, col3 ] = columns
+      col1_word = Enum.at(col1, current_row_idx)
+      col2_word = Enum.at(col2, current_row_idx)
+      col3_word = Enum.at(col3, current_row_idx)
+      new_row = [ col1_word, col2_word, col3_word ]
+      new_row = Enum.filter(new_row, & !is_nil(&1))
+      output = List.insert_at(output, current_row_idx, new_row)
+      rotate(columns, num_rows, current_row_idx+1, output)
+    else
+      output
+    end
+  end
+
   def stringColumns(words) do
-    words
-    |> String.split(" ")
-    |> Enum.sort
-    |> weight(0)
-    |> check_empty
-    |> List.flatten
-    |> columnize(0)
-    |> Enum.join
-    |> remove_last_space
-  end
+    sorted =
+      words
+      |> String.split(" ")
+      |> Enum.sort
 
-  def check_empty([h | t]) do
-    if length(h) < 3 do
-      { sec_thir_rows, rest } = Enum.split(t, 2)
-      third_row = List.last(sec_thir_rows)
-      { word , new_third_row } = Enum.split(third_row, 1)
-      first_row =
-        h
-        |> Enum.concat(word)
-        |> Enum.sort
-
-      sec_thir_rows =
-        if length(sec_thir_rows) > 1 do
-          if new_third_row == [] do
-            { output, _unused } = Enum.split(sec_thir_rows, 1)
-            output
-          else
-            List.replace_at(sec_thir_rows, -1, new_third_row)
-          end
-        else
-          [ new_third_row ]
-        end
-      new_lists = List.insert_at(sec_thir_rows, 0, first_row)
-      if rest == [] do
-        check_empty(new_lists)
-      else
-        check_empty(Enum.concat(new_lists, rest))
+    if length(sorted) == 4 do
+      [ a, b, c, d ] = sorted
+      "#{a} #{c} #{d}\n#{b}"
+    else
+      case num_rows(sorted) do
+        1 -> Enum.join(sorted, " ")
+        x ->
+          columns = Enum.chunk_every(sorted, num_rows(sorted))
+          rows = rotate(columns, num_rows(sorted), 0, [])
+          reconstruct(rows)
       end
-    else
-      [ h | t ]
-    end
-  end
-
-  def check_empty(nil) do
-    nil
-  end
-
-  def columnize([ h | t ], accum) do
-    if rem(accum, 3) == 2 do
-      [ "#{h}\n" | columnize(t, accum+1) ]
-    else
-      [ "#{h} " | columnize(t, accum+1) ]
-    end
-  end
-
-  def columnize([], _accum) do; []; end;
-
-  def remove_last_space(string) do
-    { output, _space } = String.split_at(string, -1)
-    output
-  end
-
-  def weight(list, accum) do
-    if (accum < 3) do
-      { _, next } = Enum.split(list, 1)
-      [ Enum.take_every(list, 3) | weight(next, accum + 1) ]
-    else
-      []
     end
   end
 end
@@ -78,7 +65,15 @@ end
 # IO.puts StringColumns.stringColumns("a b c d e f g h i") == "a d g\nb e h\nc f i"
 # IO.inspect StringColumns.stringColumns("a b c d e f g h")
 # IO.puts StringColumns.stringColumns("a b c d e f g h") == "a d g\nb e h\nc f"
-# IO.inspect StringColumns.check_empty(StringColumns.weight(["a", "b", "c", "d"], 0))
-# IO.inspect StringColumns.stringColumns("a b c d")
-# IO.puts StringColumns.stringColumns("a b c d") == "a c d\nb"
+IO.inspect StringColumns.stringColumns("a")
+IO.inspect StringColumns.stringColumns("a b")
 IO.inspect StringColumns.stringColumns("a b c")
+IO.inspect StringColumns.stringColumns("a b c d")
+IO.inspect StringColumns.stringColumns("a b c d e")
+# IO.puts StringColumns.stringColumns("a b c d") == "a c d\nb"
+# IO.inspect StringColumns.stringColumns("a b c")
+IO.inspect StringColumns.num_columns(["a"])
+IO.inspect StringColumns.num_columns(["a", "b"])
+IO.inspect StringColumns.num_columns(["a", "b", "c"])
+IO.inspect StringColumns.num_columns(["a", "b", "c", "d"])
+IO.inspect StringColumns.num_columns(["a", "b", "c", "a", "b", "c", "d"])
